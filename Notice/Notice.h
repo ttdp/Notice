fff0e720834a147a1a245effc823a97c581aa2e8
ttdp <tiantong.dev@gmail.com>
2019-08-07 07:57:50 +0800

    Initial commit

---
+++ Notice/Notice.h
@@ -0,0 +1,19 @@
+//
+//  Notice.h
+//  Notice
+//
+//  Created by Tian Tong on 2019/8/7.
+//  Copyright © 2019 TTDP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Notice.
+FOUNDATION_EXPORT double NoticeVersionNumber;
+
+//! Project version string for Notice.
+FOUNDATION_EXPORT const unsigned char NoticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Notice/PublicHeader.h>
+
+
